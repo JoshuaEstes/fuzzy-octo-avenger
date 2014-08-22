3996757a6ac69379654967341bcdde13f6dd8845
Joshua Estes <josh@bitpay.com>
2014-08-22 15:26:30 -0400

    Working on an interface to abstract the use of bcmath or gmp or to even just use plain ol php for math functions

---
+++ src/Bitpay/PrecisionMath/StrategyInterface.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Bitpay\Precisionmath;
+
+interface StrategyInterface
+{
+
+    /**
+     * Converts value to string
+     *
+     * @return string
+     */
+    public static function toString($a);
+
+    /**
+     * Converts value to integer, SHOULD only be used on small numbers
+     * such as a modulus return value
+     *
+     * @return integer
+     */
+    public static function toInteger($a);
+
+    /**
+     * a > b = 1
+     * a = b = 0
+     * a < b = -1
+     *
+     * @return integer
+     */
+    public static function compare($a, $b);
+
+    /**
+     * a / b
+     *
+     * @return mixed
+     */
+    public static function divide($a, $b);
+
+    /**
+     * a % b
+     *
+     * @return mixed
+     */
+    public static function modulus($a, $b);
+
+    /**
+     * Inverse Modulus
+     *
+     * @return mixed
+     */
+    public static function inverse($a, $b);
+
+    /**
+     * a * b
+     *
+     * @return mixed
+     */
+    public static function multiply($a, $b);
+
+    /**
+     * a + b
+     *
+     * @return mixed
+     */
+    public static function add($a, $b);
+
+    /**
+     * a - b
+     *
+     * @return mixed
+     */
+    public static function subtract($a, $b);
+}
